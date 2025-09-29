b134172a47344c5ed822a9ba6a30d2818b3cc0d0
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-09-29 19:42:01 +0300

    pull
    pull

--- src/app.js
+++ src/app.js
@@ -1,55 +1,3 @@
-// const input = document.querySelector("#bookmarkInput");
-// const addbBtn = document.querySelector("#addBookmarkBtn");
-// const listRef = document.querySelector("#bookmarkList");
-
-// let urlList = JSON.parse(localStorage.getItem("url")) || [];
-// const render = function () {
-//   listRef.innerHTML = urlList
-//     .map(
-//       (url, index) => `
-//       <li>
-//         <a href="${url}" target="_blank">${url}</a>
-// <button type="button" class = "delete" data-index ="${index}">Видалити</button>
-// <button type="button" class = "edit" data-index ="${index}">Редагувати</button>
-// </li>`
-//     )
-//     .join("");
-// };
-
-// const save = function () {
-//   localStorage.setItem("url", JSON.stringify(urlList));
-//   render();
-// };
-
-// addbBtn.addEventListener("click", () => {
-//   const url = input.value.trim();
-//   if (url) {
-//     urlList.push(url);
-//     input.value = "";
-//     save();
-//   }
-// });
-
-// listRef.addEventListener("click", (evt) => {
-//   const index = evt.target.dataset.index;
-//   if (evt.target.nodeName !== "BUTTON") {
-//     return;
-//   }
-//   if (evt.target.classList.contains("edit")) {
-//     const newUrl = prompt("редагуйте посилання!", urlList[index]);
-//     if (newUrl) {
-//       urlList[index] = newUrl;
-//       save();
-//     }
-//   }
-
-//   if (evt.target.classList.contains("delete")) {
-//     urlList.splice(index, 1);
-//     save();
-//   }
-// });
-
-
 const nameInput = document.querySelector("#nameInput");
 const surnameInput = document.querySelector("#surnameInput");
 const phoneInput = document.querySelector("#phoneInput");
@@ -57,20 +5,22 @@ const emailInput = document.querySelector("#emailInput");
 const addBtn = document.querySelector("#addBookmarkBtn");
 const listRef = document.querySelector("#bookmarkList");
 
-// Load from localStorage
 let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
 
 function render() {
   listRef.innerHTML = contacts
     .map(
       (c, i) => `
-      <li>
-        <strong>${c.name} ${c.surname}</strong><br>
-        📞 ${c.phone}<br>
-        ✉️ ${c.email}<br>
+     <li>
+    <div class="lis">
+        <h4 class="nun">${c.name} ${c.surname}</h4 >
+         <p class="nun">${c.phone}</p>
+         <p class="nun">${c.email}</p>
+         </div>
         <button type="button" class="edit" data-index="${i}">Редагувати</button>
         <button type="button" class="delete" data-index="${i}">Видалити</button>
-      </li>`
+      </li>
+`
     )
     .join("");
 }
@@ -80,7 +30,6 @@ function save() {
   render();
 }
 
-// Add new contact
 addBtn.addEventListener("click", () => {
   const name = nameInput.value.trim();
   const surname = surnameInput.value.trim();
@@ -90,7 +39,6 @@ addBtn.addEventListener("click", () => {
   if (name && surname && phone && email) {
     contacts.push({ name, surname, phone, email });
 
-    // clear inputs
     nameInput.value = "";
     surnameInput.value = "";
     phoneInput.value = "";
@@ -100,7 +48,6 @@ addBtn.addEventListener("click", () => {
   }
 });
 
-// Edit & delete
 listRef.addEventListener("click", (e) => {
   const index = e.target.dataset.index;
   if (!e.target.matches("button")) return;
@@ -128,5 +75,4 @@ listRef.addEventListener("click", (e) => {
   }
 });
 
-// Initial render
 render();
